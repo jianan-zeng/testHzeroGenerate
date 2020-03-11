3a3e17ae0d700f632025bd7576b52e6c4e4ec98e
jianan-zeng <1134999828@qq.com>
2020-03-11 20:09:45 +0800

    提交代码

---
+++ src/main/java/org/hzero/todo/domain/entity/Student2.java
@@ -0,0 +1,14 @@
+package org.hzero.todo.domain.entity;
+
+
+/**
+ * <p>
+ *     description
+ * @author Thinkpad 2020/03/03 14:04
+ * </p>
+ * 
+ * @author Thinkpad 2020/03/03 14:16
+ */
+public class Student {
+
+}
